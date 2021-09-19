12b7f9071ff60c43a18cc679cb5727a8511ee2b0
Keigo Tsuda <k.tsuda.103@gmail.com>
2021-09-19 23:25:35 +0900

    #38説明文をマークアップできるようにする

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <title>{{ config('app.name', '名言どうでしょう') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
